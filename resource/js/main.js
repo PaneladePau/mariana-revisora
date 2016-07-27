4be051cfa7f7ca6f86ca34a2e51c270d9a9d5522
Guilherme <guilherme.moreira@inkuba.com.br>
2016-07-27 01:19:57 -0300

    assets otimizados

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,15 +1,1 @@
-'use strict';
-
-function mobileMenu() {
-
-  $('.mobile-menu-icon').toggleClass('open');
-  $('.mobile-menu').toggleClass('open');
-  $('body').toggleClass('menu-open');
-
-}
-
-$('.mobile-menu-icon')
-  .on('click', mobileMenu);
-
-$('.mobile-menu a')
-  .on('click', mobileMenu);
+"use strict";function mobileMenu(){$(".mobile-menu-icon").toggleClass("open"),$(".mobile-menu").toggleClass("open"),$("body").toggleClass("menu-open")}$(".mobile-menu-icon").on("click",mobileMenu),$(".mobile-menu a").on("click",mobileMenu);
