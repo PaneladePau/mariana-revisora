2e96137138989e9fbdceb9aaa35f8d147d378fca
Guilherme <guilherme.moreira@inkuba.com.br>
2016-08-01 01:10:36 -0300

    form add e secoes reorganizadas

--- resource-dev/js/form.js
+++ resource-dev/js/form.js
@@ -112,18 +112,3 @@ function formSubmit(form) {
 
   }
 })();
-'use strict';
-
-function mobileMenu() {
-
-  $('.mobile-menu-icon').toggleClass('open');
-  $('.mobile-menu').toggleClass('open');
-  $('body').toggleClass('menu-open');
-
-}
-
-$('.mobile-menu-icon')
-  .on('click', mobileMenu);
-
-$('.mobile-menu a')
-  .on('click', mobileMenu);
